4dc3f99f72951e9e6168b5ed4ba682bf1c020595
misha <mishaneiman@gmail.com>
2022-04-23 14:57:32 +0300

    Fixed draw methods for shapes

--- src/PainterController.java
+++ src/PainterController.java
@@ -1,16 +1,30 @@
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
-public class PainterController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PainterController implements Initializable{
+    public ToggleButton ovalToggleBtn;
+    public MenuItem undoEditMenuItem;
+    public ToggleButton rectToggleBtn;
+    public ToggleButton lineToggleBtn;
+    public ToggleGroup shapeToggleGroup;
+    public MenuItem redoEditMenuItem;
+    public MenuItem clearEditMenuItem;
+
     private enum ShapeSelected {
-        OVAL,
-        RECTANGLE,
-        LINE
+        OVAL, RECTANGLE, LINE
     }
     private ShapeSelected shapeSelected;
     @FXML
@@ -37,45 +51,82 @@ public class PainterController {
         shapeSelected = ShapeSelected.LINE;
     }
     @FXML
-    private void drawPaneMouseClicked(MouseEvent e) {
-        switch(shapeSelected) {
-            case LINE -> {
-                currentLine = new Line(e.getX(), e.getY(), e.getX(), e.getY());
-                currentLine.setFill(fillColorPicker.getValue());
-                currentLine.setStroke(lineColorPicker.getValue());
-                drawPane.getChildren().add(currentLine);
-            }
-            case OVAL -> {
-                currentOval = new Ellipse(e.getX(), e.getY(), 0, 0);
-                currentOval.setFill(fillColorPicker.getValue());
-                currentOval.setStroke(lineColorPicker.getValue());
-                drawPane.getChildren().add(currentOval);
-            }
-            case RECTANGLE -> {
-                currentRect = new Rectangle(e.getX(), e.getY(), 0, 0);
-                currentRect.setFill(fillColorPicker.getValue());
-                currentRect.setStroke(lineColorPicker.getValue());
-                drawPane.getChildren().add(currentRect);
+    private void drawPaneMousePressed(MouseEvent e) {
+        if (shapeSelected != null) {
+            switch(shapeSelected) {
+                case LINE:
+                    currentLine = new Line(e.getX(), e.getY(), e.getX(), e.getY());
+                    currentLine.setFill(fillColorPicker.getValue());
+                    currentLine.setStroke(lineColorPicker.getValue());
+                    drawPane.getChildren().add(currentLine);
+                    break;
+                case OVAL:
+                    currentOval = new Ellipse(e.getX(), e.getY(), 0, 0);
+                    currentOval.setFill(fillColorPicker.getValue());
+                    currentOval.setStroke(lineColorPicker.getValue());
+                    drawPane.getChildren().add(currentOval);
+                    break;
+                case RECTANGLE:
+                    currentRect = new Rectangle(e.getX(), e.getY(), 0, 0);
+                    currentRect.setFill(fillColorPicker.getValue());
+                    currentRect.setStroke(lineColorPicker.getValue());
+                    drawPane.getChildren().add(currentRect);
+                    break;
             }
         }
     }
     @FXML
     private void drawPaneMouseDragged(MouseEvent e) {
-        switch(shapeSelected) {
-            case LINE -> {
-                currentLine.setEndX(e.getX());
-                currentLine.setEndY(e.getY());
-            }
-            case OVAL -> {
-                currentOval.setRadiusX(e.getX() - currentOval.getCenterX());
-                currentOval.setRadiusY(e.getY() - currentOval.getCenterY());
-            }
-            case RECTANGLE -> {
-                currentRect.setWidth(e.getX());
-                currentRect.setHeight(e.getY());
+        if (shapeSelected != null) {
+            switch (shapeSelected) {
+                case LINE:
+                    if (currentLine != null) {
+                        drawLine(e);
+                    }
+                    break;
+                case OVAL:
+                    if (currentOval != null) {
+                        drawOval(e);
+                    }
+                    break;
+                case RECTANGLE:
+                    if (currentRect != null) {
+                        drawRect(e);
+                    }
+                    break;
             }
         }
     }
+
+    private void drawRect(MouseEvent e) {
+        double dX = e.getX() - currentRect.getX();
+        double dY = e.getY() - currentRect.getY();
+        if(dX < 0) {
+            currentRect.setTranslateX(dX);
+            currentRect.setWidth(-dX);
+        } else {
+            currentRect.setTranslateX(0);
+            currentRect.setWidth(dX);
+        }
+        if (dY < 0) {
+            currentRect.setTranslateY(dY);
+            currentRect.setHeight(-dY);
+        } else {
+            currentRect.setTranslateY(0);
+            currentRect.setHeight(dY);
+        }
+    }
+
+    private void drawOval(MouseEvent e) {
+        currentOval.setRadiusX(Math.abs(e.getX() - currentOval.getCenterX()));
+        currentOval.setRadiusY(Math.abs(e.getY() - currentOval.getCenterY()));
+    }
+
+    private void drawLine(MouseEvent e) {
+        currentLine.setEndX(e.getX());
+        currentLine.setEndY(e.getY());
+    }
+
     @FXML
     private void redoEditMenuItemSelected() {
 
@@ -90,5 +141,11 @@ public class PainterController {
     @FXML
     private void clearEditMenuItemSelected() {
         drawPane.getChildren().clear();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        fillColorPicker.setValue(Color.WHITE);
+        lineColorPicker.setValue(Color.BLACK);
     }
 }
